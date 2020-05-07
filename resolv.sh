1a2a869d216e571b7d47f83a6bd8b6e350197055
luke-h1 <luke.howsam@yahoo.com>
2020-05-07 13:52:08 +0100

    init commit of resolv.sh

---
+++ resolv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+isinfile=$(cat /etc/resolv.conf | grep -c "nameserver 8.8.8.8") 
+
+if [ $isinfile -eq 0 ];
+	then
+		echo "string not in file" >> /root/nameserver.log  
+		echo "nameserver 8.8.8.8" >> /etc/resolv.conf 
+	else
+		echo "string in file" >> /root/nameserver.log 
+fi 
