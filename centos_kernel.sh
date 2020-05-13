15603a0d7542c0d868f40e667b380746760df12e
luke-h1 <luke.howsam@yahoo.com>
2020-05-13 19:00:05 +0100

    added new kernel & media scripts

--- centos_kernel.sh
+++ centos_kernel.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
+#!/bin/bash 
 
-function kernel_update { 
-	sudo yum –y update
 sudo rpm –-import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+
+
 sudo rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+
 yum list available --disablerepo='*' --enablerepo=elrepo-kernel
-sudo yum --enablerepo=elrepo-kernel install kernel-ml
+
 sudo yum --enablerepo=elrepo-kernel install kernel-lt
-reboot
-} 
 
+reboot 
 
-
-kernel_update
