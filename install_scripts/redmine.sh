3c58c4400bcf28b62852591f0c3a696bdc7772a9
luke-h1 <luke.howsam@yahoo.com>
2020-05-23 13:51:40 +0100

    added redmine.sh

---
+++ install_scripts/redmine.sh
@@ -0,0 +1,3 @@
+sudo yum install curl gpg gcc gcc-c++ make patch autoconf automake bison libffi-devel libtool  
+sudo yum install readline-devel sqlite-devel zlib-devel openssl-develh readline  glibc-headers glibc-devel
+sudo yum install mariadb-devel zlib libyaml-devel bzip2 iconv-devel ImageMagick ImageMagick-devel
