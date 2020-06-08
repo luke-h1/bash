08c6be62d7a5d95999995b03d5bdc67e63e94643
luke-h1 <luke.howsam@yahoo.com>
2020-06-08 14:26:08 +0100

    cleanup

--- maintenance-scripts/vpn-checker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -d /var/log/vpn-check/ ]; 
-then
-
-	protonvpn status >> /var/log/vpn-check/vpn.log 
-else
-	mkdir /var/log/vpn-check && protonvpn status >> /var/log/vpn-check/vpn.log 
-fi 
